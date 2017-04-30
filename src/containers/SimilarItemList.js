1e3948414fcf52e9c9fef7d8c8f0571c3441fb4b
Jonathan Bragg <jbragg@cs.washington.edu>
2017-04-30 16:32:27 -0700

    Slider

--- src/containers/SimilarItemList.js
+++ src/containers/SimilarItemList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Row, Col, Panel } from 'react-bootstrap';
+import Slider from 'react-slick';
 import { ItemThumbContainer } from '../containers/ItemContainer';
 
 const propTypes = {
@@ -8,31 +10,43 @@ const propTypes = {
   similarItemIds: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
 };
 
+const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  swipe: false,
+};
+
 const SimilarItemList = ({ primaryItemId, similarItemIds }) => (
-  <div className="panel panel-default">
-    <div className="panel-body">
-      <div className="row">
-        <div className="col-sm-3">
-          <div>
-            <strong>Next</strong>
-          </div>
-          <div className="btn-group">
-            <ItemThumbContainer draggable itemId={primaryItemId} />
-          </div>
+  <Panel>
+    <Row>
+      <Col sm={5}>
+        <div>
+          <strong>Next</strong>
         </div>
-        <div className="col-sm-9">
-          <div>
-            <strong>Similar</strong>
-          </div>
-          <div className="btn-group">
-            {similarItemIds.slice(null, 3).map(id => (
-              <ItemThumbContainer draggable itemId={id} key={id} />
-            ))}
-          </div>
+        <div className="btn-group">
+          <ItemThumbContainer draggable itemId={primaryItemId} />
         </div>
-      </div>
-    </div>
-  </div>
+      </Col>
+      <Col sm={7}>
+        <div>
+          <strong>Similar</strong>
+        </div>
+        <Slider {...sliderSettings}>
+          {similarItemIds.length > 0
+            ? similarItemIds.map(id => (
+            <div key={id}>
+              <ItemThumbContainer draggable itemId={id} />
+            </div>
+            ))
+            : <div />
+          }
+        </Slider>
+      </Col>
+    </Row>
+  </Panel>
 );
 
 SimilarItemList.propTypes = propTypes;
