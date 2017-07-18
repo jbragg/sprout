98d24e3fdfff2e0679182bedabe278601d27c92d
Jonathan Bragg <jbragg@cs.washington.edu>
2017-07-18 11:53:34 -0700

    Simplify production build to fix bugs

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -3,6 +3,4 @@ mkdir -p src/static/js
 browserify ./src/index.js \
     -g [envify --NODE_ENV 'production'] \
     -t [ babelify ] \
-    -g uglifyify \
-    -p bundle-collapser/plugin \
     | uglifyjs --compress --mangle -o src/static/js/bundle.js
